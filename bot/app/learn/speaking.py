39561e952e932a0c3d43cd7f99d607be94ae1737
tezer <taras8055@gmail.com>
2019-11-05 22:00:26 +0200

    Voice recognition is a subscription feature

--- bot/app/learn/speaking.py
+++ bot/app/learn/speaking.py
@@ -1,6 +1,11 @@
-from bot.app.core import authorize, bot
+from bot.app.core import authorize, bot, TOKEN
 from loguru import logger
 from aiogram import types
+
+from bot.app.learn.control import do_learning
+from bot.bot_utils.bot_utils import compare
+from bot.ilt import level_up
+from bot.speech import speech2text
 
 
 async def voice_message(message: types.Message):
@@ -8,6 +13,12 @@ async def voice_message(message: types.Message):
     if not isValid:
         return
     logger.info(str(message.from_user.id) + ' voice message received')
+    if not session.subscribed:
+        await bot.send_message(message.from_user.id, "You need to buy subscription "
+                                                     "to be able to use voice recognition."
+                                                     "\n Use command /subscribe to buy subscription")
+
+        return
     file = await bot.get_file(message.voice.file_id)
     url = 'https://api.telegram.org/file/bot{}/'.format(TOKEN)
     url = url + file["file_path"]
