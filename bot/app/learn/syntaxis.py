d676667e6bf7b2ec7063a4c95a0958f602d4f3d0
tezer <taras8055@gmail.com>
2019-11-11 11:37:56 +0200

    Clean up and typo fix

--- bot/app/learn/syntaxis.py
+++ bot/app/learn/syntaxis.py
@@ -8,29 +8,28 @@ async def unscramble_message(query, callback_data):
     session, isValid = await authorize(query.from_user.id)
     if not isValid:
         return
-    revield = session.unscramble_revieled
+    reveald = session.unscramble_revieled
     m = query.message
     new_data = list()
     new_keys = list()
     for i in range(len(session.unscramble_data)):
         n = session.unscramble_data[i]
         if n == int(callback_data['data']):
-            revield = revield + ' ' + session.unscramble_keys[i]
+            revield = reveald + ' ' + session.unscramble_keys[i]
             continue
         new_data.append(n)
         new_keys.append(session.unscramble_keys[i])
-    await do_unscramble(session, new_keys, new_data, session.unscramble_sentence, revield, m)
+    await do_unscramble(session, new_keys, new_data, session.unscramble_sentence, reveald, m)
 
 
 
-async def do_unscramble(session, keys, data, sentence, revieled, message):
+async def do_unscramble(session, keys, data, sentence, reveald, message):
     actions = ['unscramble'] * len(data)
     k = bot_utils.to_vertical_keyboard(keys, data, actions)
-    # await message.edit_reply_markup(k)
-    await message.edit_text("*" + sentence[1] + "*" + "\n" + revieled, reply_markup=k)
+    await message.edit_text("*" + sentence[1] + "*" + "\n" + reveald, reply_markup=k)
     session.unscramble_keys = keys
     session.unscramble_data = data
-    session.unscramble_revieled = revieled
+    session.unscramble_revieled = reveald
     session.unscramble_sentence = sentence
 
 
